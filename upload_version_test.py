5359ddcb440521c58b0da272c39e3876884e0af0
estrunnikova <estrunnikova@team.amocrm.com>
2024-04-01 00:32:44 +0300

    add README.md and comments

--- upload_version_test.py
+++ upload_version_test.py
@@ -11,9 +11,11 @@ df.dropna(inplace=True)
 rename_field_values = df.columns.tolist()[9]
 df = df.rename(columns={rename_field_values: rename_field_values.strip()})
 
-df['values'] = df['values'].str.replace(',', '.').astype(float)  # Тоже преобразовала во float, так как это численное значение, которое в string нам вряд ли нужно держать.
+# 'values' тоже преобразовала во float, так как это численное значение, которое в string нам вряд ли нужно держать.
+df['values'] = df['values'].str.replace(',', '.').astype(float)
 df['Корень'] = df['Корень'].str.replace(',', '.').astype(float)
 
+# В качестве пропущенной функции по заданию не чётко ясно, что должно быть. Предполагаю сбрасывание индекса.
 df.groupby(['x1', 'x2', 'x3', 'y1', 'y2', 'y3', 'z1', 'z2', 'z3', 'values', 'Корень'], sort=False).sum().reset_index()
 df_2 = pd.DataFrame({'x1': ['x1 1'], 'new_columns': 'ok'})
 new_dataframe = pd.merge(df, df_2, on="x1")
